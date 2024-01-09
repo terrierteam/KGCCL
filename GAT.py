5b4a77286cc7e82ab61b52c9b21b8154bc61d52d
zeyuanmeng <71137064+zeyuanmeng@users.noreply.github.com>
2024-01-09 18:14:48 +0000

    Create GAT.py

---
+++ GAT.py
@@ -0,0 +1,88 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.init import ones_
+
+class GAT(nn.Module):
+    def __init__(self, nfeat, nhid, dropout, alpha):
+        """Dense version of GAT."""
+        super(GAT, self).__init__()
+        self.dropout = dropout
+        self.layer = GraphAttentionLayer(nfeat, nhid, dropout=dropout, alpha=alpha, concat=False)
+
+    def forward(self, item_embs, entity_embs, adj):
+        x = F.dropout(item_embs, self.dropout, training=self.training)
+        y = F.dropout(entity_embs, self.dropout, training=self.training)
+        x = self.layer(x, y, adj)
+        x = F.dropout(x, self.dropout, training=self.training)
+        return x
+    
+    def forward_relation(self, item_embs, entity_embs, w_r, adj):
+        x = F.dropout(item_embs, self.dropout, training=self.training)
+        y = F.dropout(entity_embs, self.dropout, training=self.training)
+        x = self.layer.forward_relation(x, y, w_r, adj)
+        x = F.dropout(x, self.dropout, training=self.training)
+        return x
+
+class GraphAttentionLayer(nn.Module):
+    def __init__(self, in_features, out_features, dropout, alpha, concat=True):
+        super(GraphAttentionLayer, self).__init__()
+        self.dropout = dropout
+        self.in_features = in_features
+        self.out_features = out_features
+        self.alpha = alpha
+        self.concat = concat
+        self.W = nn.Parameter(torch.empty(size=(in_features, out_features)))
+        nn.init.xavier_uniform_(self.W.data, gain=1.414)
+        self.a = nn.Parameter(torch.empty(size=(2*out_features, 1)))
+        nn.init.xavier_uniform_(self.a.data, gain=1.414)
+        self.fc = nn.Linear(2*out_features, out_features)
+        self.leakyrelu = nn.LeakyReLU(self.alpha)
+
+    def forward_relation(self, item_embs, entity_embs, relations, adj):
+        Wh = item_embs.unsqueeze(1).expand(entity_embs.size())
+        We = entity_embs
+        a_input = torch.cat((Wh,We),dim=-1) # (N, e_num, 2*dim)
+        e_input = torch.multiply(self.fc(a_input), relations).sum(-1) # N,e
+        e = self.leakyrelu(e_input) # (N, e_num)
+        zero_vec = -9e15*torch.ones_like(e)
+        attention = torch.where(adj > 0, e, zero_vec)
+        attention = F.softmax(attention, dim=1)
+        attention = F.dropout(attention, self.dropout, training=self.training) # N, e_num
+        entity_emb_weighted = torch.bmm(attention.unsqueeze(1), entity_embs).squeeze()
+        h_prime = entity_emb_weighted+item_embs
+        if self.concat:
+            return F.elu(h_prime)
+        else:
+            return h_prime
+
+    def forward(self, item_embs, entity_embs, adj):
+        Wh = torch.mm(item_embs, self.W) # h.shape: (N, in_features), Wh.shape: (N, out_features)
+        We = torch.matmul(entity_embs, self.W) # entity_embs: (N, e_num, in_features), We.shape: (N, e_num, out_features)
+        a_input = self._prepare_cat(Wh, We) # (N, e_num, 2*out_features)
+        e = self.leakyrelu(torch.matmul(a_input, self.a).squeeze(2)) # (N, e_num)
+        zero_vec = -9e15*torch.ones_like(e)
+        attention = torch.where(adj > 0, e, zero_vec)
+        attention = F.softmax(attention, dim=1)
+        attention = F.dropout(attention, self.dropout, training=self.training) # N, e_num
+        entity_emb_weighted = torch.bmm(attention.unsqueeze(1), entity_embs).squeeze()
+        h_prime = entity_emb_weighted+item_embs
+        if self.concat:
+            return F.elu(h_prime)
+        else:
+            return h_prime
+          
+    def _prepare_cat(self, Wh, We):
+        Wh = Wh.unsqueeze(1).expand(We.size()) # (N, e_num, out_features)
+        return torch.cat((Wh, We), dim=-1) # (N, e_num, 2*out_features)
+
+    def _prepare_attentional_mechanism_input(self, Wh):
+        N = Wh.size()[0] # number of nodes
+        Wh_repeated_in_chunks = Wh.repeat_interleave(N, dim=0)
+        Wh_repeated_alternating = Wh.repeat(N, 1)
+        all_combinations_matrix = torch.cat([Wh_repeated_in_chunks, Wh_repeated_alternating], dim=1)
+        return all_combinations_matrix.view(N, N, 2 * self.out_features)
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' + str(self.in_features) + ' -> ' + str(self.out_features) + ')'
